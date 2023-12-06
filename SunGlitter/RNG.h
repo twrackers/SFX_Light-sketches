754df3f7aa792219ab9ca1c89b5e585188d8b741
Thomas W Rackers <twrackers@users.noreply.github.com>
2023-12-05 22:56:01 -0500

    Comment RNG.h

--- SunGlitter/RNG.h
+++ SunGlitter/RNG.h
@@ -3,16 +3,31 @@
 
 #include <stdint.h>
 
+// Random Number Generator
+// Returns random values in half-open range [0, (2^31)-1),
+// Works on processor (Arduino) that doesn't support 64-bit integers.
+
 class RNG {
 
   private:
-    uint32_t m_state;
+    uint32_t m_state;    // internal state value
 
   public:
+    // Constructor
     RNG();
+    // Set state to seed value
     void seed(const uint32_t x);
+    // Return random value from full range [0, (2^31)-1)
+    // @return random value
     uint32_t random();
+    // Return random value from range [0, mx)
+    // @param mx maximum value (exclusive) of returned range
+    // @return random value
     uint32_t random(const uint32_t mx);
+    // Return random value from range [mn, mx)
+    // @param mn minimum value (inclusive) of returned range
+    // @param mx maximum value (exclusive) of returned range
+    // @return random value
     uint32_t random(const uint32_t mn, const uint32_t mx);
   
 };
